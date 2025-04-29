97a783cdd9d3ff09aeda88f30257a2021825fb64
Jayshree Anandakumar <ping.jayshree@gmail.com>
2025-04-29 16:01:56 +0530

    Contract deployed

--- contracts/src/StakeSenseVault.sol
+++ contracts/src/StakeSenseVault.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
 /**
  * @title StakeSenseVault
@@ -184,6 +184,6 @@ contract StakeSenseVault is AccessControl, ReentrancyGuard, Pausable {
 
     // Receive function to accept ETH
     receive() external payable {
-        deposit();
+        this.deposit{value: msg.value}();
     }
 } 
